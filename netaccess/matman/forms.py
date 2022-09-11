b0c2831d423a3914f80b45b5ebc853d2c2b59b2c
slarag <58182961+Slarag@users.noreply.github.com>
2022-09-11 20:32:00 +0200

    Lots of changes, new features

--- netaccess/matman/forms.py
+++ netaccess/matman/forms.py
@@ -7,10 +7,35 @@ from . import models
 class BorrowForm(forms.ModelForm):
     class Meta:
         model = models.Borrow
-        fields = []
+        fields = ['usage_location']
 
 
 class SearchForm(forms.Form):
     query = forms.CharField(
         widget=forms.TextInput(attrs={'autofocus': True})
     )
+
+
+class MaterialForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['scheme'].queryset = models.Scheme.objects.filter(is_active=True)
+
+    class Meta:
+        model = models.Material
+        fields = ['serial_number', 'material_number', 'manufacturer', 'description', 'scheme', 'owner', 'tags',
+                  'is_active']
+
+
+PictureFormset = forms.inlineformset_factory(models.Material, models.MaterialPicture,
+                                             fields=['file', 'description'], extra=5, max_num=5, can_order=True)
+
+
+class SettingsForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['default_scheme'].queryset = models.Scheme.objects.filter(is_active=True)
+
+    class Meta:
+        model = models.UserProfile
+        fields = ['default_scheme']
--- netaccess/netaccess/urls.py
+++ netaccess/netaccess/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +26,6 @@ urlpatterns = [
     path('', include('matman.urls')),
     path('', RedirectView.as_view(pattern_name='home')), #, permanent=True)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
