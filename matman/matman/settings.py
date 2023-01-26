dc4b0f0da42797ad9a083ee98780e26abc725aaf
slarag <58182961+Slarag@users.noreply.github.com>
2023-01-26 18:34:34 +0100

    Refactored settings back into module, added daphne to requirements

--- matman/matman/settings.py
+++ matman/matman/settings.py
@@ -10,9 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 import os.path
 
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -21,10 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -78,7 +77,12 @@ WSGI_APPLICATION = 'matman.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
@@ -133,3 +137,21 @@ CELERY_TASK_TIME_LIMIT = 30 * 60
 
 ALLOW_REGISTRATION = True
 ALLOW_CHANGE_PASSWORD = True
+
+ADMINS = (
+    ('CHANGEME', 'CHANGEME@CHANGEME.com'),
+)
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+# SECURE_HSTS_SECONDS = 3600
+
+# Read SECRET_KEY from an environment variable
+SECRET_KEY = os.environ['SECRET_KEY']
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.environ.get('DJANGO_DEBUG', '').lower() == 'true'
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
