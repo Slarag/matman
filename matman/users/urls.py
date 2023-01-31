05004980989e2b39a66924597b2647c12611853c
Michael <58182961+Slarag@users.noreply.github.com>
2023-01-31 10:14:03 +0100

    updated url parameter for password_reset_confirm

--- matman/users/urls.py
+++ matman/users/urls.py
@@ -18,8 +18,9 @@ if getattr(settings, 'ALLOW_CHANGE_PASSWORD', False):
         ), name='password-change'),
         path('password/reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
         path('password/reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-        path('password/reset/confirm/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+        path('password/reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
         path('password/reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+...
     ]
 
 if getattr(settings, 'ALLOW_REGISTRATION', False):
