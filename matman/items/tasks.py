c1b2d1fe1f729787978211bc9adbd7f4a8cf3579
slarag <58182961+Slarag@users.noreply.github.com>
2023-04-23 18:57:45 +0200

    Cleaned code in items/tasks.py
    Signed-off-by: slarag <58182961+Slarag@users.noreply.github.com>

--- matman/items/tasks.py
+++ matman/items/tasks.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+from typing import Any
 
 from django.utils.timezone import now
 from django.core import mail
@@ -15,117 +16,107 @@ from .models import Borrow, Item, Comment
 
 User = get_user_model()
 
+
+def check_if_send_notification(user: User | None) -> bool:
+    """
+    Check if notification can/should be sent to a certain User.
+
+    Check if:
+        - user is not None
+        - user has an email address registered
+        - user is active
+
+    :param user: User to be checked. Also accepts None value.
+    :return: True, if notification should be sent to the given user, else false.
+    """
+
+    return all([
+        user is not None,
+        user.email,
+        user.active
+    ])
+
+
 @shared_task
 def send_item_notifications(item_pk: int, created: bool, editor_pk: int | None):
-    item = Item.objects.get(pk=item_pk)
-    editor = User.objects.get(pk=editor_pk)
-    context = {
+    item: Item = Item.objects.get(pk=item_pk)
+    editor: User = User.objects.get(pk=editor_pk)
+    context: dict[str, Any] = {
         'item': item,
         'created': created,
         'editor': editor,
         'site': Site.objects.get_current(),
     }
-    from_email = settings.DEFAULT_FROM_EMAIL
-    subject = f'MatMan - Item {item.identifier} {"created" if created else "updated"}'
+    from_email: str = settings.DEFAULT_FROM_EMAIL
+    subject: str = f'MatMan - Item {item.identifier} {"created" if created else "updated"}'
+    notified_users: set[User] = {item.owner, editor}
 
-    if item.owner is not None and item.owner.email:
-        # Send notification to owner
-        context['recipient'] = item.owner
-        html_message = render_to_string('items/mail/item_notification.html', context)
-        plain_message = strip_tags(html_message)
-        to = item.owner.email
-        mail.send_mail(subject, plain_message, from_email, [to], html_message=html_message)
-    if editor is not None and editor != item.owner and editor.email:
-        # Send notification to creator
-        context['recipient'] = editor
-        html_message = render_to_string('items/mail/item_notification.html', context)
-        plain_message = strip_tags(html_message)
-        to = editor.email
-        mail.send_mail(subject, plain_message, from_email, [to], html_message=html_message)
+    for user in notified_users:
+        if not check_if_send_notification(user):
+            continue
+        context['recipient'] = user
+        html_message: str = render_to_string('items/mail/item_notification.html', context)
+        plain_message: str = strip_tags(html_message)
+        mail.send_mail(subject=subject, message=plain_message, from_email=from_email, recipient_list=[user.email],
+                       html_message=html_message)
 
 
 @shared_task
 def send_comment_notifications(comment_pk: int, created: bool, editor_pk: int | None):
-    comment = Comment.objects.get(pk=comment_pk)
-    editor = User.objects.get(pk=editor_pk)
-    context = {
+    comment: Comment = Comment.objects.get(pk=comment_pk)
+    editor: User = User.objects.get(pk=editor_pk)
+    context: dict[str, Any] = {
         'item': comment.item,
         'comment': comment,
         'created': created,
         'editor': editor,
         'site': Site.objects.get_current(),
     }
-    item = comment.item
-    from_email = settings.DEFAULT_FROM_EMAIL
-    subject = f'MatMan - Comment on {item.identifier} {"created" if created else "updated"}'
-
-    if item.owner is not None and item.owner.email:
-        # Send notification to owner
-        context['recipient'] = item.owner
-        html_message = render_to_string('items/mail/comment_notification.html', context)
-        plain_message = strip_tags(html_message)
-        to = item.owner.email
-        mail.send_mail(subject, plain_message, from_email, [to], html_message=html_message)
-    if editor is not None and editor != item.owner and editor.email:
-        # Send notification to creator
-        context['recipient'] = editor
-        html_message = render_to_string('items/mail/comment_notification.html', context)
-        plain_message = strip_tags(html_message)
-        to = editor.email
-        mail.send_mail(subject, plain_message, from_email, [to], html_message=html_message)
+    item: Item = comment.item
+    from_email: str = settings.DEFAULT_FROM_EMAIL
+    subject: str = f'MatMan - Comment on {item.identifier} {"created" if created else "updated"}'
+    notified_users: set[User] = {item.owner, editor}
     if item.is_borrowed:
-        borrow = item.active_borrow
-        borrowed_by = borrow.borrowed_by
-        if borrowed_by not in (item.owner, editor) and borrowed_by.email:
-            # Also send email to person who has currently borrowed the item
-            context['recipient'] = borrowed_by
-            html_message = render_to_string('items/mail/comment_notification.html', context)
-            plain_message = strip_tags(html_message)
-            to = borrowed_by.email
-            mail.send_mail(subject, plain_message, from_email, [to], html_message=html_message)
+        notified_users.add(item.active_borrow.borrowed_by)
+
+    for user in notified_users:
+        if not check_if_send_notification(user):
+            continue
+        context['recipient'] = user
+        html_message: str = render_to_string('items/mail/comment_notification.html', context)
+        plain_message: str = strip_tags(html_message)
+        mail.send_mail(subject=subject, message=plain_message, from_email=from_email, recipient_list=[user.email],
+                       html_message=html_message)
 
 
 @shared_task
 def send_borrow_notifications(borrow_pk: int, created: bool, returned: bool, editor_pk: int | None):
-    borrow = Borrow.objects.get(pk=borrow_pk)
-    editor = User.objects.get(pk=editor_pk)
-    context = {
+    borrow: Borrow = Borrow.objects.get(pk=borrow_pk)
+    editor: User = User.objects.get(pk=editor_pk)
+    context: dict[str, Any] = {
         'borrow': borrow,
         'created': created,
         'returned': returned,
         'editor': editor,
         'site': Site.objects.get_current(),
     }
-    item = borrow.item
-    from_email = settings.DEFAULT_FROM_EMAIL
+    item: Item = borrow.item
+    from_email: str = settings.DEFAULT_FROM_EMAIL
+    subject: str = f'MatMan - Borrow for item {item.identifier} updated'
     if created:
         subject = f'MatMan - Item {item.identifier} borrowed'
     elif returned:
         subject = f'MatMan - Item {item.identifier} returned'
-    else:
-        subject = f'MatMan - Borrow for item {item.identifier} updated'
+    notified_users: set[User] = {item.owner, editor, borrow.borrowed_by}
 
-    if item.owner is not None and item.owner.email:
-        # Send notification to owner
-        context['recipient'] = item.owner
-        html_message = render_to_string('items/mail/borrow_notification.html', context)
-        plain_message = strip_tags(html_message)
-        to = item.owner.email
-        mail.send_mail(subject, plain_message, from_email, [to], html_message=html_message)
-    if editor is not None and editor != item.owner and editor.email:
-        # Send notification to creator
-        context['recipient'] = editor
-        html_message = render_to_string('items/mail/borrow_notification.html', context)
-        plain_message = strip_tags(html_message)
-        to = editor.email
-        mail.send_mail(subject, plain_message, from_email, [to], html_message=html_message)
-    if borrow.borrowed_by not in (editor, item.owner) and borrow.borrowed_by.email:
-        # Send notification to creator
-        context['recipient'] = borrow.borrowed_by
-        html_message = render_to_string('items/mail/borrow_notification.html', context)
-        plain_message = strip_tags(html_message)
-        to = borrow.borrowed_by.email
-        mail.send_mail(subject, plain_message, from_email, [to], html_message=html_message)
+    for user in notified_users:
+        if not check_if_send_notification(user):
+            continue
+        context['recipient'] = user
+        html_message: str = render_to_string('items/mail/borrow_notification.html', context)
+        plain_message: str = strip_tags(html_message)
+        mail.send_mail(subject=subject, message=plain_message, from_email=from_email, recipient_list=[user.email],
+                       html_message=html_message)
 
 
 @shared_task
@@ -145,9 +136,9 @@ def send_reminders():
             # Nothing to do, continue with next user
             continue
 
-        html_message = render_to_string('items/mail/borrow_reminder.html', context)
-        plain_message = strip_tags(html_message)
-        from_email = settings.DEFAULT_FROM_EMAIL
-        to = user.email
-        mail.send_mail(subject, plain_message, from_email, [to], html_message=html_message)
+        html_message: str = render_to_string('items/mail/borrow_reminder.html', context)
+        plain_message: str = strip_tags(html_message)
+        from_email: str = settings.DEFAULT_FROM_EMAIL
+        mail.send_mail(subject=subject, message=plain_message, from_email=from_email, recipient_list=[user.email],
+                       html_message=html_message)
 
